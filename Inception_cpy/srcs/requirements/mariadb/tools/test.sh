62efa12044d1b501a052a06df9d2fff4843514f6
jakob <jakobklocker@hotmail.com>
2023-08-07 17:50:43 +0200

    nearly final

--- Inception_cpy/srcs/requirements/mariadb/tools/test.sh
+++ Inception_cpy/srcs/requirements/mariadb/tools/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "CREATE DATABASE IF NOT EXISTS mydb;" > tmp.sql
-echo "CREATE USER IF NOT EXISTS 'jakob'@'%' IDENTIFIED BY '12345678';" >> tmp.sql
-echo "GRANT ALL PRIVILEGES ON mydb.* TO 'jakob'@'%';" >> tmp.sql
+echo "CREATE DATABASE IF NOT EXISTS $db_name;" > tmp.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pass';" >> tmp.sql
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';" >> tmp.sql
 echo "FLUSH PRIVILEGES;" >> tmp.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345678';" >> tmp.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_pass';" >> tmp.sql
 echo "FLUSH PRIVILEGES;" >> tmp.sql
 
 service mysql start
--- Inception_cpy/srcs/requirements/wordpress/tool/wp_script.sh
+++ Inception_cpy/srcs/requirements/wordpress/tool/wp_script.sh
@@ -10,27 +10,20 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
  #To be able to type just wp, make the file executable and move it to somewhere in your PATH
  chmod +x wp-cli.phar
  mv wp-cli.phar /usr/local/bin/wp
- 
- #Create run directory : 
- 
 
- 
  #download wordpress, always use --allow-root so we dont have to create a user
   wp core download --allow-root
   
   #cp the sample config we copied to correct folder
   cp /wp-config.php /var/www/html/
   
-  echo $db_name $db_user $db_pass
-  
   #replace, make sure to use double quotes otherwise env var wont get replaced
   sed -i "s/database_name_here/$db_name/g" wp-config.php
   sed -i "s/username_here/$db_user/g" wp-config.php
   sed -i "s/password_here/$db_pass/g" wp-config.php
   sed -i "s/localhost/mariadb/g" wp-config.php
-  
-  cat wp-config.php
-  
+
+  wait-for-it.sh mariadb:3306 -t 60
   #instal homepage, create user, install a theme
   wp core install --url=$domain_name --title=jklockerPage --admin_user=$wp_admin --admin_password=$wp_adminpass --admin_email=$wp_adminmail --allow-root
   wp user create $wp_user $wp_email --role=contributor --user_pass=$wp_pass --allow-root
